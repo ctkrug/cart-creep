db6ccb816cc07c9e38e30bc8d588464f2e8cafa1
ctkrug <ctkrug4501@gmail.com>
2026-07-13 02:52:24 +0000

    chore: add eslint flat config

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        localStorage: "readonly",
+      },
+    },
+  },
+  {
+    files: ["tests/**/*.js"],
+    languageOptions: {
+      globals: {
+        describe: "readonly",
+        it: "readonly",
+        expect: "readonly",
+        beforeEach: "readonly",
+      },
+    },
+  },
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
+];
